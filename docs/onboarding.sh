960b77cb3c7813b3ebda14db029dacf525cfc97e
Humair Khan <humair88@hotmail.com>
2022-10-20 12:46:33 -0400

    Add script.

---
+++ docs/onboarding.sh
@@ -0,0 +1,6 @@
+git clone https://x-access-token:${GITHUB_TOKEN}@github.com/${ORG_NAME}/${REPO_NAME}
+cd ${REPO_NAME}
+git checkout -b onboarding_${PROJECT-NAME}
+kustomize init
+git add -u
+git commit -m "onboarding pr."
