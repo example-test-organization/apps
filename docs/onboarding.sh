680ff360a92843bceea67b8dd1e3a32b6e401700
Humair Khan <humair88@hotmail.com>
2022-11-01 12:04:31 -0400

    test

--- docs/onboarding.sh
+++ docs/onboarding.sh
@@ -1,6 +1,7 @@
 git checkout -b onboarding_${PROJECT-NAME}
 echo ${TARGET_REPO}, ${SCRIPT_PATH}
 cat /mnt/shared/data.yaml
+yq /mnt/shared/data.yaml
 kustomize init
 git add -u
 git commit -m "onboarding pr."
