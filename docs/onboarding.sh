8307991d7847876dc5953772873e7a1bac549bbe
Humair Khan <humair88@hotmail.com>
2022-11-01 13:28:06 -0400

    test

--- docs/onboarding.sh
+++ docs/onboarding.sh
@@ -1,7 +1,17 @@
-git checkout -b onboarding_${PROJECT-NAME}
-echo ${TARGET_REPO}, ${SCRIPT_PATH}
-cat /mnt/shared/data.yaml
-yq /mnt/shared/data.yaml
+# Pre-conditions:
+# Working directory is cloned apps repo
+#
+
+PAYLOAD=${WORKING_DIR}/data.yaml
+PROJECT=$(yq e .project-name ${PAYLOAD})
+QUOTA=$(yq e .quota[0] ${PAYLOAD})
+
+git checkout -b onboarding_${PROJECT}
+echo Repo: ${TARGET_REPO}
+
+echo Users:
+yq e '.users = (.users | split(",")) ' data.yaml
+
 kustomize init
 git add -u
 git commit -m "onboarding pr."
