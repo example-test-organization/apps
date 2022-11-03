283648d923e60d9cf43282d04d88e7d5348cf8a7
Humair Khan <humair88@hotmail.com>
2022-11-03 18:21:49 -0400

    update

--- docs/onboarding.sh
+++ docs/onboarding.sh
@@ -8,7 +8,7 @@ cd ${WORKING_DIR}/apps
 echo Repo: ${TARGET_REPO}
 
 echo Users:
-yq e '.users = (.users | split(",")) ' ${PAYLOAD}
+yq '.users | split(",") | map(trim)' -o json -I=0 ${PAYLOAD}
 
 kustomize init
 git add kustomization.yaml
--- scripts/onboard.sh
+++ scripts/onboard.sh
@@ -4,13 +4,25 @@ PROJECT=$(yq e .project-name ${PAYLOAD})
 ENVIRONMENT=moc
 CLUSTER=$(yq e .cluster[0] ${PAYLOAD})
 NAMESPACE=$(yq e .project-name ${PAYLOAD})
-REQUESTER=gingersnap
-DISPLAY_NAME="gingersnap"
-PROJECT_OWNER=gingersnap
+REQUESTER=$(yq e .project-owner ${PAYLOAD})
+DISPLAY_NAME=$(yq e .project-name ${PAYLOAD})
+PROJECT_OWNER=$(yq e .project-owner ${PAYLOAD})
 ONBOARDING_ISSUE=https://github.com/${ORG_NAME}/${SOURCE_REPO}/issues/${ISSUE_NUMBER}
-DOCS=https://github.com/gingersnap-cloud
-GROUP=gingersnap
-USERS="['ryanemerson', 'pminz', 'tristantarrant', 'karesti', 'wburns', 'fax4ever', 'rigazilla', 'jabolina', 'pruivo', 'dpanshug']"
+DOCS=$(yq e .project-docs-link ${PAYLOAD})
+GROUP=$(yq e .team-name ${PAYLOAD})
+USERS=$(yq '.users | split(",") | map(trim)' -o json -I=0 ${PAYLOAD})
+
+
+echo ENVIRONMENT $ENVIRONMENT
+echo CLUSTER $CLUSTER
+echo NAMESPACE $NAMESPACE
+echo REQUESTER $REQUESTER
+echo DISPLAY_NAME $DISPLAY_NAME
+echo PROJECT_OWNER $PROJECT_OWNER
+echo ONBOARDING_ISSUE $ONBOARDING_ISSUE
+echo DOCS $DOCS
+echo GROUP $GROUP
+echo USERS $USERS
 
 scripts/create_jsonnet_group.sh
 scripts/create_namespace.sh
